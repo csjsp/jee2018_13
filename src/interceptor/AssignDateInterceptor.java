4f81016a5a4c2644deb21c73235317df3eec151d
csjsp <cs_jsp@126.com>
2018-04-20 14:40:14 +0800

    利用拦截器为action赋值

--- src/interceptor/AssignDateInterceptor.java
+++ src/interceptor/AssignDateInterceptor.java
@@ -1,14 +1,23 @@
 package interceptor;
 
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.opensymphony.xwork2.ActionInvocation;
 import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
 
-public class AssignDateInterceptor extends AbstractInterceptor {
+import controller.MyAction;
 
+public class AssignDateInterceptor extends AbstractInterceptor {
+  private final static Logger logger=LoggerFactory.getLogger(AssignDateInterceptor.class);
   @Override
-  public String intercept(ActionInvocation arg0) throws Exception {
-    // TODO Auto-generated method stub
-    return null;
+  public String intercept(ActionInvocation arg) throws Exception {
+    MyAction myAction=(MyAction)arg.getAction();
+    myAction.setDate(new Date());
+    logger.info(""+myAction.getDate());
+    return arg.invoke();
   }
 
 }
