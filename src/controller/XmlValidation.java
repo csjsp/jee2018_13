6cc9c6ea2f1ba7b7b3889182ee143f779f6a7488
csjsp <cs_jsp@126.com>
2018-05-02 11:33:14 +0800

    1 怎么输入日期 2 怎么校验最大和最小

--- src/controller/XmlValidation.java
+++ src/controller/XmlValidation.java
@@ -1,12 +1,15 @@
 package controller;
 
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 import com.opensymphony.xwork2.ActionSupport;
 
 public class XmlValidation extends ActionSupport {
   int test_int;
   double test_double;
   String pass,repass;
-
+  Date test_date_jquery;
   public int getTest_int() {
     return test_int;
   }
@@ -38,5 +41,19 @@ public class XmlValidation extends ActionSupport {
   public void setRepass(String repass) {
     this.repass = repass;
   }
-  
+
+public Date getTest_date_jquery() {
+	return test_date_jquery;
+}
+
+public void setTest_date_jquery(Date test_date_jquery) {
+	this.test_date_jquery = test_date_jquery;
+}
+public Date getMaxDate(){
+	return new GregorianCalendar(2020,0,1).getTime();
+}
+public Date getMinDate(){
+	return new GregorianCalendar(2000,0,1).getTime();
+}
+
 }
